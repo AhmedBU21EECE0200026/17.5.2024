411b35b9ea99841f1fdc36e1ee535ccf45801511
AhmedBU21EECE0200026 <138860410+AhmedBU21EECE0200026@users.noreply.github.com>
2024-05-17 07:11:45 +0000

    change to three

--- 3_implementation/Untitled-2.c
+++ 3_implementation/Untitled-2.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
  int main(){
-    int num1, num2;
+    int num1, num2 ,num3;
     int result;
-    printf("enter two numbers:");
-    scanf("%d %d", &num1 , &num2);
-    result=num1 + num2;
-    printf("sum of %d and %d is %d\n",num1,num2,result);
+    printf("enter three numbers:");
+    scanf("%d %d", &num1 , &num2 ,&num3);
+    result=num1 + num2 +num3;
+    printf("%d + %d + %d = %d\n",num1,num2,result);
     return 0;
  }
